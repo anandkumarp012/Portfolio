b8f315ac4570769d65f77140196ee27a98be67be
P. Anand Kumar <anandkumarp093@gmail.com>
2025-05-14 18:21:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    strings:["Frontend Developer","Backend Developer","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
